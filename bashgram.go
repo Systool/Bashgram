1cfab2f186b79a75a477167a4c0e498458fe145e
Systool <systemtool.exe@gmail.com>
2019-04-20 20:53:21 +0200

    Initial Commit

---
+++ bashgram.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const tgEndpoint = "https://api.telegram.org/bot711908048:AAGiRadEwO3cG93QtPKCn8ebBn2dj3JFPEU/"
+
+func main() {
+	sigchan := make(chan os.Signal, 2)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGKILL)
+	var lastUpd uint
+	if resp, err := http.Get(tgEndpoint + "getUpdates"); err == nil {
+		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+			p := make([]APIResponse, 10)
+			json.NewDecoder(resp.Body).Decode(&p)
+			resp.Body.Close()
+			for i := 0; i < len(p); i++ {
+				/*if p[i].Result > lastUpd {
+					lastUpd
+				}*/
+			}
+		}
+	} else {
+		panic(err.Error())
+	}
+	for {
+		select {
+		case _ = <-sigchan:
+			break
+		default:
+			if resp, err := http.Get(tgEndpoint + "getUpdates"); err == nil {
+				if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+					p := APIResponse{}
+					json.NewDecoder(resp.Body).Decode(&p)
+					if p.Ok {
+						fmt.Println(p)
+					}
+					resp.Body.Close()
+				}
+			}
+		}
+	}
+}
---
+++ type.go
@@ -0,0 +1,35 @@
+package main
+
+import "encoding/json"
+
+type APIResponse struct {
+	Ok     bool            `json:"ok"`
+	Result json.RawMessage `json:"result"`
+}
+
+type Update struct {
+	UpdateID int      `json:"update_id"`
+	Message  *Message `json:"message"`
+}
+
+type Message struct {
+	MessageID int    `json:"message_id"`
+	From      *User  `json:"from"`
+	Date      int    `json:"date"`
+	Chat      *Chat  `json:"chat"`
+	Text      string `json:"text"`
+}
+
+type Chat struct {
+	ID   int64  `json:"id"`
+	Type string `json:"type"`
+}
+
+type User struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`     // optional
+	UserName     string `json:"username"`      // optional
+	LanguageCode string `json:"language_code"` // optional
+	IsBot        bool   `json:"is_bot"`        // optional
+}
